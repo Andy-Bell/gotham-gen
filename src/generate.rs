f2bc74b64a9bf35739390abdd71e02e229a4c5aa
Andy Bell <andy.bell.github@gmail.com>
2018-08-31 17:24:25 +0100

    Start to work on matching args.

---
+++ src/generate.rs
@@ -0,0 +1,4 @@
+use clap::ArgMatches;
+
+pub fn run(matches: ArgMatches) {}
+
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,16 @@ mod cli;
 pub fn run_gen() {
     let matches = cli::build_gen().get_matches();
     println!("Matches = {:?}", matches);
+
+    match matches.subcommand() {
+        ("new", Some(_)) => {
+            new::run(matches);
+        },
+        ("generate", Some(_)) => {
+            generate::run(matches);
+        },
+        _ => { println!("No subcommand");}
+    };
 }
 
 
