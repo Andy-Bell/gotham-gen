8258c3d5aa06a415563e4eb6cc76be3a55355d53
Andy Bell <andy.bell.github@gmail.com>
2018-09-04 17:27:53 +0100

    WIP: Templating

--- src/new.rs
+++ src/new.rs
@@ -1,5 +1,13 @@
 use clap::ArgMatches;
+use std::fs;
 
 pub fn run(matches: &ArgMatches) {
     println!("Matches = {:?}", matches);
+    let test = fs::read_to_string("./src/templates/cargo.toml.txt")
+        .expect("No file found")
+        .as_str()
+        .replace("{name}", "test");
+    print!("test: {}",test);
+    fs::create_dir("test").unwrap();
+    fs::write("test/cargo.toml", test.as_bytes()).unwrap();
 }
